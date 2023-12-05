cf2452034859948d977207921827a2ea4500b1f5
axschech <ax.schech@gmail.com>
2023-12-05 02:21:35 +0000

    still going

--- index.ts
+++ index.ts
@@ -13,6 +13,12 @@ const VALID_NUMBERS = [
     "nine"
 ];
 
+const VALID_INPUTS = [
+    ...Array.from(Array(10).keys()),
+    ...VALID_NUMBERS
+]
+
+console.log({VALID_INPUTS})
 const makeMap = () => {
     return VALID_NUMBERS.map((number, index) => {
         return {
@@ -24,7 +30,7 @@ const makeMap = () => {
 const makeRegex = () => {
     const numbersInReg = Object.values(VALID_NUMBERS).join("|");
     
-    return new RegExp(`([\s\S]*?)(?!${numbersInReg})`, "g");
+    return new RegExp(`(${numbersInReg}|\\\d)`, "g");
 }
 
 async function main() {
@@ -33,13 +39,24 @@ async function main() {
     
     const reg = makeRegex();
 
-    // console.log(reg);
+    console.log(reg);
     const items = input.split(/\r?\n|\r|\n/g);
+      const found = items.reduce((prev, item) => {
 
-    const found = items.reduce((prev, item) => {
-        const numbers = item.replaceAll(reg, "");
-        console.log(numbers)
-        return prev + (+`${numbers.at(0)}${numbers.at(numbers.length -1)}`);
+       const matches = [...item.matchAll(reg)].map((match) => {
+        return {
+            found: match[0],
+            start: match.index,
+            end: (match.index || 0) + match[0].length,
+            input: match.input
+        }
+       }).sort((a, b) => {
+            const start = a.start || a.end;
+            const end = b.start || b.end;
+            return start > end ? 1 : -1;
+       });
+       console.log(matches, "matches");
+       return prev;
     }, 0)
 
     return found;
